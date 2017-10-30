d7d416c111e071e447c33eac6bcb868ec32a34c3
Lalita Desouza <lmdesou@yahoo.com>
2017-10-30 18:58:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  var newObject = Object.assign( {}, object, {[key]: value})
+  var newObject = Object.assign( {}, obj)
   return
 }
 
